9992b6b30def3cab64552f85ab75f78c120260d3
ragul-n <raguln@Raguls-MacBook-Air.local>
2023-09-27 23:59:23 +0530

    Adding files

---
+++ 01_get_subtype.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+df=pd.read_csv('data/KP-ASH-1821.csv')
+df['subtype']=df[df.columns[9:]].apply(
+    lambda x:''.join(x.dropna().astype(str)),
+    axis=1
+)
+
+
+df3 = df.groupby(['Year','Country','subtype'])['subtype'].count()
+
+df2.to_csv('ATS-subtype5-con.csv')
+
